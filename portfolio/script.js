9a62cde73e088c9df2151695956c70604630b3c0
Maria Luiza Brito Laranjeira <91203745+malulb@users.noreply.github.com>
2024-05-16 10:42:59 -0300

    Delete portfolio directory

--- portfolio/script.js
+++
@@ -1,7 +0,0 @@
-function toggleMenu() {
-    const menu = document.querySelector(".burger-menu-links");
-    const icon = document.querySelector(".burger-menu-icon");
-
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-}
